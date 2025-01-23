3f88f54ae82423f5f3380410f780ee78c898bd88
Hungryvietdude <s2300809@edu.bc.fi>
2025-01-23 09:44:50 +0200

    Ovi tehtävä ja ruokaannos

---
+++ Ovitehrävä/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Ovitehrävä
+{
+    internal class Program
+    {
+        // Luettelo oven tiloille
+        enum OvenTila
+        {
+            Lukossa,
+            Kiinni,
+            Auki
+        }
+
+        static void Main(string[] args)
+        {
+            // Oven tila
+            OvenTila ovenTila = OvenTila.Lukossa;
+
+            // Pääluuppi
+            while (true)
+            {
+                // Näytä nykyinen tila
+                Console.WriteLine($"Ovi on {ovenTila}. Mitä haluat tehdä?");
+
+                // Kysy komento
+                string komento = Console.ReadLine()?.ToLower();
+
+                // Tarkista komento ja päivitä oven tila
+                switch (komento)
+                {
+                    case "poista lukitus":
+                        if (ovenTila == OvenTila.Lukossa)
+                        {
+                            ovenTila = OvenTila.Kiinni;
+                            Console.WriteLine("Lukitus poistettu.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ovi ei ole lukossa, joten lukitusta ei voi poistaa.");
+                        }
+                        break;
+
+                    case "avaa":
+                        if (ovenTila == OvenTila.Kiinni)
+                        {
+                            ovenTila = OvenTila.Auki;
+                            Console.WriteLine("Ovi avattiin.");
+                        }
+                        else if (ovenTila == OvenTila.Lukossa)
+                        {
+                            Console.WriteLine("Ovi on lukossa. Poista lukitus ennen avaamista.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ovi on jo auki.");
+                        }
+                        break;
+
+                    case "sulje":
+                        if (ovenTila == OvenTila.Auki)
+                        {
+                            ovenTila = OvenTila.Kiinni;
+                            Console.WriteLine("Ovi suljettiin.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ovi ei ole auki, joten sitä ei voi sulkea.");
+                        }
+                        break;
+
+                    case "lukitse":
+                        if (ovenTila == OvenTila.Kiinni)
+                        {
+                            ovenTila = OvenTila.Lukossa;
+                            Console.WriteLine("Ovi lukittiin.");
+                        }
+                        else if (ovenTila == OvenTila.Auki)
+                        {
+                            Console.WriteLine("Ovi on auki. Sulje ovi ennen lukitsemista.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ovi on jo lukossa.");
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Tuntematon komento. Yritä uudelleen.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Ruoka Annos Generaattori/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+enum PääRaakaAine
+{
+    Nautaa,
+    Kanaa,
+    Kasviksia
+}
+
+enum Lisuke
+{
+    Perunaa,
+    Riisiä,
+    Pastaa
+}
+
+enum Kastike
+{
+    Pippuri,
+    Chili,
+    Tomaatti,
+    Curry
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Pääraaka-aine (nautaa, kanaa, kasviksia):");
+        PääRaakaAine pääaine = ValitseRaakaAine<PääRaakaAine>();
+
+        Console.WriteLine("Lisukkeet (perunaa, riisiä, pastaa):");
+        Lisuke lisuke = ValitseRaakaAine<Lisuke>();
+
+        Console.WriteLine("Kastike (pippuri, chili, tomaatti, curry):");
+        Kastike kastike = ValitseRaakaAine<Kastike>();
+
+        Console.WriteLine($"\n{pääaine} ja {lisuke} {kastike}-kastikkeella");
+    }
+
+    static T ValitseRaakaAine<T>() where T : Enum
+    {
+        while (true)
+        {
+            string syöte = Console.ReadLine()?.Trim().ToLower();
+            foreach (T arvo in Enum.GetValues(typeof(T)))
+            {
+                if (syöte == arvo.ToString().ToLower())
+                {
+                    return arvo;
+                }
+            }
+            Console.WriteLine("Virheellinen syöte. Yritä uudelleen:");
+        }
+    }
+}
