113fca6f757e8cd23b30f1edef2bb9ff91afeebd
Hungryvietdude <s2300809@edu.bc.fi>
2025-03-17 17:14:14 +0200

    Pelikehitys Tehtävä 7 väri luokat

---
+++ Värit/Värit/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Tavara
+{
+    public override string ToString()
+    {
+        return GetType().Name;
+    }
+}
+
+public class Miekka : Tavara { }
+public class Jousi : Tavara { }
+public class Kirves : Tavara { }
+
+public class VaritettyTavara<T> where T : Tavara
+{
+    private T tavara;
+    private ConsoleColor vari;
+
+    public VaritettyTavara(T tavara, ConsoleColor vari)
+    {
+        this.tavara = tavara;
+        this.vari = vari;
+    }
+
+    public void NaytaTavara()
+    {
+        Console.ForegroundColor = vari;
+        Console.WriteLine(tavara);
+        Console.ResetColor();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        VaritettyTavara<Miekka> sininenMiekka = new VaritettyTavara<Miekka>(new Miekka(), ConsoleColor.Blue);
+        VaritettyTavara<Jousi> punainenJousi = new VaritettyTavara<Jousi>(new Jousi(), ConsoleColor.Red);
+        VaritettyTavara<Kirves> vihreaKirves = new VaritettyTavara<Kirves>(new Kirves(), ConsoleColor.Green);
+
+        sininenMiekka.NaytaTavara();
+        punainenJousi.NaytaTavara();
+        vihreaKirves.NaytaTavara();
+    }
+}
